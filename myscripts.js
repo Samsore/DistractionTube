ed5b00ea49a2c9b61f68a89a337f8c037837ff24
Samsore <shamssamsore@yahoo.ca>
2017-05-27 15:32:55 -0400

    Replaces video thumbnails, and added new blockthumbnail2.png

--- myscripts.js
+++ myscripts.js
@@ -1,23 +1,24 @@
 //document.body.innerHTML = document.body.innerHTML.replace(new RegExp("Swift 3 Tutorial", "g"), "Removed Text");
 
 var myElement = document.getElementById("eow-title");
-alert(myElement.innerHTML)
-myElement.innerHTML = "Removed Title"
-
-var relatedVideos = document.getElementsByClassName("stat view-count");
-var i;
-for (i = 0; i< relatedVideos.length; i++){
-	if (relatedVideos[i].innerHTML == "Recommended for you"){
-		relatedVideos[i].innerHTML += "REMOVE THIS VIDEO";
-		relatedVideos[i].previousElementSibling.innerHTML = "REMOVE CHANNEL";
-		relatedVideos[i].previousElementSibling.previousElementSibling.previousElementSibling.innerHTML = "REMOVE TITLE";
-		var gpElement = relatedVideos[i].parentNode.parentNode;
-		var thumbnailElement = gpElement.nextElementSibling.firstElementChild.firstElementChild.firstElementChild;
-		//alert(thumbnailElement);
-		//thumbnailElement.src = chrome.extension.getURL("blockthumbnail.png");
-		thumbnailElement.setAttribute("src", "");
-		//thumbnailElement.setAttribute("src", chrome.extension.getURL("blockthumbnail.png"));
-		//alert("prev" + relatedVideos[i].nextSibling);
-		//relatedVideos.nextSibling.innerHTML = "REMOVE CHANNEL";
+myElement.innerHTML = "Removed Title";
+blockThumbnail();
+function blockThumbnail(){
+	var relatedVideos = document.getElementsByClassName("stat view-count");
+	var i;
+	for (i = 0; i< relatedVideos.length; i++){
+		if (relatedVideos[i].innerHTML.startsWith("Recommended for you")){
+			relatedVideos[i].innerHTML = "This was a distracting recommended video.";
+			relatedVideos[i].previousElementSibling.innerHTML = "This was the distracting channel";
+			relatedVideos[i].previousElementSibling.previousElementSibling.previousElementSibling.innerHTML = "This was the distracting title";
+			var gpElement = relatedVideos[i].parentNode.parentNode;
+			var thumbnailElement = gpElement.nextElementSibling.firstElementChild.firstElementChild.firstElementChild;
+			thumbnailElement.setAttribute("src", chrome.extension.getURL("blockthumbnail2.png"));
+			thumbnailElement.setAttribute("data-thumb", chrome.extension.getURL("blockthumbnail2.png"));
+		}
 	}
+}
+
+function removeLinks(){
+	
 }
